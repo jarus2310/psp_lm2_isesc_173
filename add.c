15dc179cf67c4b95df514999b189101fc1aabf35
Suraj Kunte <110719308+jarus2310@users.noreply.github.com>
2022-08-06 17:52:22 +0530

    edit 4

--- add.c
+++ add.c
@@ -1,16 +1,25 @@
+/******************************************************************************
+
+                            Online C Compiler.
+                Code, Compile, Run and Debug C program online.
+Write your code in this editor and press "Run" button to compile and execute it.
+
+*******************************************************************************/
+
 #include <stdio.h>
-int add(int x,int y,int z)
+int add(int x,int y,int z,int m)
 {
-    int s=x+y+z;
+    int s=x+y+z+m;
     return s;
 }
 int main()
 
 {
-    int a,b,c,sum;
+    int a,b,c,d,sum;
     printf("Enter the numbers to be added\n");
-    scanf("%d%d%d",&a,&b,&c);
-    sum=add(a,b,c);
-    printf("%d+%d+%d=%d",a,b,c,sum);
+    scanf("%d%d%d%d",&a,&b,&c,&d);
+    sum=add(a,b,c,d);
+    printf("%d+%d+%d+%d=%d",a,b,c,d,sum);
     return 0;
 }
+
