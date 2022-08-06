be83e2bfd45073a84c7693d4e47357ec97c3d7ad
Suraj Kunte <110719308+jarus2310@users.noreply.github.com>
2022-08-06 16:36:46 +0530

    edit 2

--- add.c
+++ add.c
@@ -2,10 +2,11 @@
 #include<math.h>
 int main()
 {
+    /* local variable declaration */
   int a,b,sum;
   printf("enter the two numbers for addition\n");
-  scanf("%d%d",&a,&b);
-  sum=a+b;
-  printf("the sum of %d and %d is %d",a,b,sum);
+  scanf("%d%d",&a,&b);                                    //reading the input
+  sum=a+b;                                                //equation for sum of two no.s 
+  printf("the sum of %d and %d is %d",a,b,sum);           //displaying the sum 
   return 0;
 }                     
