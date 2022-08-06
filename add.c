46feff085a336221130fe47f7aa2f2731af691ea
Suraj Kunte <110719308+jarus2310@users.noreply.github.com>
2022-08-06 16:22:24 +0530

    Create add.c

---
+++ add.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include<math.h>
+int main()
+{
+  int a,b,sum;
+  printf("enter the two numbers for addition");
+  scanf("%d%d",&a,&b);
+  sum=a+b;
+  printf("%d",sum);
+  return 0;
+}
