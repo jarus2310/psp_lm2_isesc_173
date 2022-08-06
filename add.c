5b5e4743158b9c10273cbd17bcdc660b146a991a
Suraj Kunte <110719308+jarus2310@users.noreply.github.com>
2022-08-06 17:43:04 +0530

    edit 3

--- add.c
+++ add.c
@@ -1,12 +1,16 @@
 #include <stdio.h>
-#include<math.h>
-int main()
+int add(int x,int y,int z)
 {
-    /* local variable declaration */
-  int a,b,sum;
-  printf("enter the two numbers for addition\n");
-  scanf("%d%d",&a,&b);                                    //reading the input
-  sum=a+b;                                                //equation for sum of two no.s 
-  printf("the sum of %d and %d is %d",a,b,sum);           //displaying the sum 
-  return 0;
-}                     
+    int s=x+y+z;
+    return s;
+}
+int main()
+
+{
+    int a,b,c,sum;
+    printf("Enter the numbers to be added\n");
+    scanf("%d%d%d",&a,&b,&c);
+    sum=add(a,b,c);
+    printf("%d+%d+%d=%d",a,b,c,sum);
+    return 0;
+}
